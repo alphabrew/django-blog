3bc613ef6a08d65a142e0473e0417d6b3a81731c
alphabrew <steven.harbin@gmail.com>
2021-02-11 06:21:35 -0500

    updates

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from . views import HomeView
-from . views import ArticleDetailView, AddPostView
+from . views import ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('addpost', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),
+    path('article/delete/<int:pk>', DeletePostView.as_view(), name='delete_post'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . models import Post
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 #def home(request):
@@ -8,13 +11,31 @@ from . models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    #ordering = ['-id']
+    ordering = ['-post_date']
 
 class ArticleDetailView(DetailView):
     model= Post
     template_name = 'article_details.html'
 
-class AddPostView(CreateView):
+class AddPostView(LoginRequiredMixin, CreateView):
     model= Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields='__all__'
+    login_url = '/members/login/'
+
+    #fields='__all__'
     #fields= ['title', 'body']
+
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'editpost.html'
+    login_url = '/members/login/'
+    #fields = ['title', 'body']
+
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'deletepost.html'
+    login_url = '/members/login/'
+    success_url = reverse_lazy('home')
